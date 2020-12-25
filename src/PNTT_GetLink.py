c78c2e88e249b830b4c7fd57f919ad709fb5ffa9
CuongLe <lvcuong210@gmail.com>
2020-12-25 09:55:41 +0700

    Update: Added dir for folder

--- src/PNTT_GetLink.py
+++ src/PNTT_GetLink.py
@@ -4,10 +4,11 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import subprocess
 import os
+import download
 
 def getlinks():
     allLinkFiles = list()
-    driver = webdriver.Chrome("D:\\Github\\Python\\ChangeProperties\\PNTT\\chromedriver.exe")
+    driver = webdriver.Chrome(download.saveFile() + "\\IDE\\chromedriver.exe")
     # to maximize the browser window
     driver.minimize_window()
     driver.get("https://truyenaudio.org/kiem-hiep/nghe-truyen/pham-nhan-tu-tien-truyen-tien-hiep.html")
@@ -17,7 +18,7 @@ def getlinks():
     print("**************************************************************************************")
 
     click = driver.find_elements_by_class_name("download-item")
-    path = "D:\\Github\\Python\\ChangeProperties\\PNTT\\test.sh"
+    path = download.saveFile() + "\\src\\test.sh"
 
     if os.path.exists(path):
         os.remove(path)
@@ -26,11 +27,14 @@ def getlinks():
 
     pause = 200
     for i in range(200, 466):
+        
         if i == (pause + 5):
             f.write("echo -p \"---------------> PAUSED <--------------------\"\n")
             pause += 5
+            os.system("pause")
         temp = click[i].get_attribute("href")
         f.write("start \"" + temp + "\"\n")
+        download.downloadLink(temp)
         allLinkFiles.append(temp)
     f.close()
 
--- src/download.py
+++ src/download.py
@@ -2,13 +2,21 @@ import os
 import re
 import glob
 from mutagen.easyid3 import EasyID3
+import getpass
+import os.path
+
+def saveFile():
+    path = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+    return str(path)
 
 def downloadLink(URL):
     PathIDM = "C:\\Program Files (x86)\\Internet Download Manager\\IDMan.exe"
-    os.system('"{}" /d {} /n'.format(PathIDM, URL))
+    os.system('"{}" /d {} /n /p {}'.format(PathIDM, URL, saveFile()  + "\\MP3"))
 
 def renameFile():
-    dirPath = "C:\\Users\\Le Van Cuong\\Downloads\\Music\\"
+    username = getpass.getuser()
+    print(username)
+    dirPath = saveFile()  + "\\MP3\\"
     files = glob.glob(dirPath + "*.mp3")
     
     #rename file:
